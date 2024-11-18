4e5d9634f5a93d7448c1420ed3d22f432e0ae4a5
Mario Camelo <mariuo@gmail.com>
2024-11-18 15:58:37 -0500

    DemoWithCallBack

--- kafka-basics/src/main/java/com/camelo/kafka/ProducerDemoWithCallBack.java
+++ kafka-basics/src/main/java/com/camelo/kafka/ProducerDemoWithCallBack.java
@@ -1,8 +1,6 @@
 package com.camelo.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +8,8 @@ import org.slf4j.LoggerFactory;
 import java.util.Properties;
 
 
-public class ProducerDemo {
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+public class ProducerDemoWithCallBack {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallBack.class.getSimpleName());
 
     public static void main(String[] args) {
         log.info("I am a Kafka Producer");
@@ -22,6 +20,9 @@ public class ProducerDemo {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer .class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+//        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "400");
+//        properties.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, RoundRobinPartitioner.class.getName());
 
 //        // connect to Conduktor Playground
 //        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
@@ -36,13 +37,38 @@ public class ProducerDemo {
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
+
 //        String topic = "wikimedia.recentchange";
 
-        // create producer records
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
 
-        // send data
-        producer.send(producerRecord);
+
+        for(int j = 0; j<10 ; j++) {
+            for (int i = 0; i < 30; i++) {
+                // create producer records
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world " + i);
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        //executes every time a record successfully sent or an exception is thrown
+                        if (e == null) {
+                            log.info("Received new metadata \n +" +
+                                    "Topic: " + metadata.topic() + "\n" +
+                                    "Partition: " + metadata.partition() + "\n" +
+                                    "Offsets: " + metadata.offset() + "\n" +
+                                    "Timestamp: " + metadata.timestamp());
+                        } else {
+                            log.error("Error while producing", e);
+                        }
+                    }
+                });
+            }
+        }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         // tell the producer to send all data block until done -- synchronous
         producer.flush();
